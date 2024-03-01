70603e47637e73b27605dd340767f9d78f52f0f9
Xhufg <Minardi.alessio99@gmail.com>
2024-03-01 21:55:23 +0100

    0.0.1 organize homepage

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -1,21 +1,22 @@
 import React from "react";
-import {Button} from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 
 export default function Homepage() {
     return (
         <>
-        <div className="grid grid-cols-3 gap-4 mt-8">
 
-            <div></div>
-
-            <div></div>
-
-            <div> 
-                <Button color="primary" variant="ghost" size="lg" className="text-xl">
-                    Are you an Escrow Agent?
-                </Button>
+            <div className="grid grid-cols-3 gap-4 mt-12 ml-24 mr-24">
+                <div className="col"></div>
+                <div className="col flex justify-center items-center">
+                    <Button color="primary" variant="ghost" size="lg" className="text-xl sm:text-sm md:text-base">
+                        Are you an Escrow Agent?
+                    </Button>
+                </div>
+                <div className="col"></div>
+                <div className="col rounded p-4 py-48 border border-black-400 rounded-md shadow-md "></div>
+                <div className="col"></div>
+                <div className="col rounded p-4 py-48 border border-black-400 rounded-md shadow-md "></div>
             </div>
-
-        </div>
         </>
-    )}
+    )
+}
